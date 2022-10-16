d843738ad820e48cb886a09c1e98ca63934a98d5
MuhammadHuzaifak2025 <115894335+MuhammadHuzaifak2025@users.noreply.github.com>
2022-10-16 20:28:19 +0500

    Update and rename Question 05.c to Question 5.c

--- Question 5.c
+++ Question 5.c
@@ -1,20 +1,22 @@
 #include<stdio.h>
 #include<math.h>
 main(){
-	int hour;
-	float per_ice_cost, days,hours, a,quan_ice;
+	int hour, days,quan_ice;
+	float per_ice_cost, a;
+	double hours;
 	per_ice_cost = 1.2;
 	printf("Enter how much hours you want machine to operate: ");
 	scanf("%d",&hour);
 	printf("Enter the quatnity of icecream: ");
-	scanf("%f",&quan_ice);
+	scanf("%d",&quan_ice);
 	if (hour == 8){
 		int price, ice_per_cost;
 		days = (quan_ice / 50)/8;
-		hours = ((quan_ice / 50))%8;
+		hours = (quan_ice %400);
+		hours = hours / 8;
 //	a= quan_ice/50;
 //		hours = a-days;w
-		printf("%f\n%f\n",days,abs(hours));
+		printf("%d\n%.0f\n",days,ceil(hours));
 		price=per_ice_cost*quan_ice;
 		printf("%d",price);
 	}
